737a92bbfad8fc88db78c663a438a703e35adab7
Ingo <ingogums@hotmail.com>
2023-01-15 11:10:28 -0300

    Full unit test coverage (except parseLine)

--- src/test/java/br/com/ingoltda/capitalgain/controllers/CliControllerTest.java
+++ src/test/java/br/com/ingoltda/capitalgain/controllers/CliControllerTest.java
@@ -1,6 +1,5 @@
 package br.com.ingoltda.capitalgain.controllers;
 
-import br.com.ingoltda.capitalgain.models.Operation;
 import br.com.ingoltda.capitalgain.services.OperationLineProcessorService;
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,9 +10,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.ByteArrayInputStream;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 import static org.mockito.ArgumentMatchers.any;
 
